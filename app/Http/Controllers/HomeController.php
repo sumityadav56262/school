aef5a52be6f81df93ce9453afbd87685e7a6d296
SK SINGH <singhsushil5277@gmail.com>
2025-06-28 15:33:51 +0545

    Updated HomeController
    Optimized the code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Teacher;
+use App\Models\Student;
+use App\Models\StudentFee;
+use App\Models\TeacherExpense;
+use App\Models\MiscExpense;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $totalStudents = \App\Models\Student::count();
-        $totalTeachers = \App\Models\Teacher::count();
-        $totalIncome =  \App\Models\StudentFee::sum('payment');
-        $totalDues =  \App\Models\StudentFee::sum('dues');
-        $totalExpenses = \App\Models\TeacherExpense::sum('paid_amt') +  \App\Models\MiscExpense::sum('amount'); // or \App\Models\Expense if available
+        $totalStudents  =   Student::count();
+        $totalTeachers  =   Teacher::count();
+        $totalIncome    =   StudentFee::sum('payment');
+        $totalDues      =   StudentFee::sum('dues');
+        $totalExpenses  =   TeacherExpense::sum('paid_amt') +  MiscExpense::sum('amount'); // or \App\Models\Expense if available
 
         return view('dashboard', compact(
             'totalStudents',
