02d87b94c8e795f071548245912f9e2b1ee3a5a2
SK SINGH <singhsushil5277@gmail.com>
2025-06-28 21:50:13 +0545

    Added search functionality misc exp.

--- resources/views/misc_expenses/index.blade.php
+++ resources/views/misc_expenses/index.blade.php
@@ -8,33 +8,49 @@
             <a href="{{ route('misc-expenses.create') }}" class="reset-button">Add Expense</a>
         </div>
         <div class="search-group">
-            <input class="search-box" type="text">
-            <button>Search</button>
+            <form method="GET" action="{{ route('misc-expenses.index') }}">
+                <input type="text" class="search-box" name="search" value="{{ request('search') }}" placeholder="Search...">
+                <button type="submit">Search</button>
+            </form>
         </div>
     </div>
-    <table border="1" width="100%" style="margin-top: 10px;">
-        <tr>
-            <th>Particular</th>
-            <th>Amount</th>
-            <th>Paid By</th>
-            <th>Date</th>
-            <th>Actions</th>
-        </tr>
-        @foreach($expenses as $exp)
-        <tr>
-            <td>{{ $exp->particular }}</td>
-            <td>{{ $exp->amount }}</td>
-            <td>{{ $exp->payment_by }}</td>
-            <td>{{ $exp->payment_date }}</td>
-            <td>
-                <a href="{{ route('misc-expenses.edit', $exp) }}">Edit</a>
-                <form action="{{ route('misc-expenses.destroy', $exp) }}" method="POST" style="display:inline;">
-                    @csrf @method('DELETE')
-                    <button type="submit" >Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
+
+    <table>
+        <thead>
+            <tr>
+                <th>Particular</th>
+                <th>Amount</th>
+                <th>Paid By</th>
+                <th>Date</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($expenses as $exp)
+                <tr>
+                    <td>{{ $exp->particular }}</td>
+                    <td>{{ $exp->amount }}</td>
+                    <td>{{ $exp->payment_by }}</td>
+                    <td>{{ $exp->payment_date }}</td>
+                    <td>
+                        <a class="edit-button" href="{{ route('misc-expenses.edit', $exp) }}">Edit</a>
+                        <form action="{{ route('misc-expenses.destroy', $exp) }}" method="POST" style="display:inline;">
+                            @csrf 
+                            @method('DELETE')
+                            <button type="submit" class="delete-button">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="5" style="text-align: center;">No expenses found.</td>
+                </tr>
+            @endforelse
+        </tbody>
     </table>
+
+    <div style="margin-top: 10px;">
+        {{ $expenses->links() }}
+    </div>
 </div>
 @endsection
