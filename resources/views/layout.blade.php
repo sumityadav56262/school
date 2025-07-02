3ceaa26188104cdea78dd0d456844a3bee1c93f9
SK SINGH <singhsushil5277@gmail.com>
2025-07-02 16:06:19 +0545

    Resolve bootstrap integration issue & Updated CSS and UI
    -Deleted unused welcome.blade.php file -Improved UI issue
    -Removed inline css from layout and dashboard

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,39 +22,39 @@
 </head>
 
 <body>
-    <nav class="navbar">
-        <h1>SCHOOL NAME</h1>
+    <nav class="text-center text-white c_nav">
+        <h1>Sapience Academy</h1>
     </nav>
     <div class="container">
         <div class="menu">
             <a class="menu-item {{ request()->routeIs('dashboard') ? 'active' : '' }}" href="{{ route('dashboard') }}">
-                <i class="fas fa-tachometer-alt" style="margin-right: 8px;"></i>
+                <i class="fas fa-tachometer-alt"></i>
                 <span>Dashboard</span>
             </a>
             <a class="menu-item {{ request()->routeIs('students.*') ? 'active' : '' }}"
                 href="{{ route('students.index') }}">
-                <i class="fas fa-user-graduate" style="margin-right: 8px;"></i>
+                <i class="fas fa-user-graduate"></i>
                 <span>Students</span>
             </a>
             <a class="menu-item {{ request()->routeIs('student-fees.*') ? 'active' : '' }}"
                 href="{{ route('student-fees.index') }}">
-                <i class="fas fa-file-invoice-dollar" style="margin-right: 8px;"></i>
+                <i class="fas fa-file-invoice-dollar"></i>
                 <span>Student Fees</span>
             </a>
             <a class="menu-item {{ request()->routeIs('teachers.*') ? 'active' : '' }}"
                 href="{{ route('teachers.index') }}">
-                <i class="fas fa-chalkboard-teacher" style="margin-right: 8px;"></i>
+                <i class="fas fa-chalkboard-teacher"></i>
                 <span>Teachers</span>
             </a>
             <a class="menu-item {{ request()->routeIs('teacher-expenses.*') ? 'active' : '' }}"
                 href="{{ route('teacher-expenses.index') }}">
-                <i class="fas fa-money-bill-wave" style="margin-right: 8px;"></i>
-                <span>Teach Expenses</span>
+                <i class="fas fa-money-bill-wave"></i>
+                <span>Teacher Exps</span>
             </a>
             <a class="menu-item {{ request()->routeIs('misc-expenses.*') ? 'active' : '' }}"
                 href="{{ route('misc-expenses.index') }}">
-                <i class="fas fa-coins" style="margin-right: 8px;"></i>
-                <span>Misc Expenses</span>
+                <i class="fas fa-coins"></i>
+                <span>Misc Exps</span>
             </a>
         </div>
 
