81ff93a11a6df48768317f8b5a25b786cb8410f6
SK SINGH <singhsushil5277@gmail.com>
2025-07-02 22:25:01 +0545

    Added emis no field in student fee index file & re-order other fields

--- resources/views/student_fees/form.blade.php
+++ resources/views/student_fees/form.blade.php
@@ -1,12 +1,22 @@
 <div class="form-row">
     <div class="form-group">
+        <label for="emis_no">EMIS No:</label>
+        <input type="number" name="emis_no" id="emis_no" value="{{ old('emis_no', $studentFee->emis_no ?? '') }}">
+    </div>
+    <div class="form-group">
+        <label for="paid_date">Date:</label>
+        <input type="text" name="paid_date" id="paid_date" readonly
+            value="{{ old('paid_date', $studentFee->paid_date ?? $todayFormattedBsDate) }}" disabled>
+    </div>
+</div>
+<div class="form-row">
+    <div class="form-group">
         <label for="class_name">Class:</label>
         <select name="class_name">
             @foreach ($classNames as $className)
                 <option value="{{ $className->class_name }}"> {{ $className->class_name }} </option>
             @endforeach
         </select>
-
     </div>
     <div class="form-group">
         <label for="roll_no">Roll No:</label>
@@ -16,14 +26,16 @@
 
 <div class="form-row">
     <div class="form-group">
+        <div class="form-group">
+            <label for="sname">Full Name:</label>
+            <input type="text" name="sname" id="sname" value="{{ old('sname', $studentFee->sname ?? '') }}"
+                disabled>
+        </div>
+    </div>
+    <div class="form-group">
         <label for="adm_date">Adm. Date:</label>
         <input class="datepicker-here" type="text" data-language="nep"
             value="{{ old('adm_date', $studentFee->adm_date ?? '') }}">
-    </div>
-    <div class="form-group">
-        <label for="paid_date">Date:</label>
-        <input type="text" name="paid_date" id="paid_date" readonly
-            value="{{ old('paid_date', $studentFee->paid_date ?? $todayFormattedBsDate) }}">
     </div>
 </div>
 
@@ -48,19 +60,19 @@
         <input type="number" name="yearly" id="yearly" value="{{ old('yearly', $studentFee->yearly ?? 0) }}">
     </div>
     <div class="form-group">
-        <label for="tb">TB:</label>
-        <input type="number" name="tb" id="tb" value="{{ old('tb', $studentFee->tb ?? 0) }}">
+        <label for="monthly">Monthly:</label>
+        <input type="number" name="monthly" id="monthly" value="{{ old('monthly', $studentFee->monthly ?? 0) }}">
     </div>
 </div>
 
 <div class="form-row">
     <div class="form-group">
-        <label for="monthly">Monthly:</label>
-        <input type="number" name="monthly" id="monthly" value="{{ old('monthly', $studentFee->monthly ?? 0) }}">
-    </div>
-    <div class="form-group">
         <label for="vest">Vest:</label>
         <input type="number" name="vest" id="vest" value="{{ old('vest', $studentFee->vest ?? 0) }}">
+    </div>
+    <div class="form-group">
+        <label for="tb">TB:</label>
+        <input type="number" name="tb" id="tb" value="{{ old('tb', $studentFee->tb ?? 0) }}">
     </div>
 </div>
 
@@ -89,11 +101,12 @@
 <div class="form-row">
     <div class="form-group">
         <label for="misc_fee">Misc Fee:</label>
-        <input type="number" name="misc_fee" id="misc_fee" value="{{ old('misc_fee', $studentFee->misc_fee ?? 0) }}">
+        <input type="number" name="misc_fee" id="misc_fee"
+            value="{{ old('misc_fee', $studentFee->misc_fee ?? 0) }}">
     </div>
     <div class="form-group">
         <label for="previous-dues-checkbox">Previous Dues</label>
-        <input type="number" value="0" readonly>
+        <input type="number" value="0" disabled>
     </div>
 </div>
 
@@ -102,26 +115,26 @@
 <div class="form-row">
     <div class="form-group">
         <label for="total_amt">Total:</label>
-        <input type="number" name="total_amt" id="total_amt" readonly
+        <input type="number" name="total_amt" id="total_amt" disabled
             value="{{ old('total_amt', $studentFee->game ?? 0) }}">
     </div>
-    <div class="form-group">
-        <label for="discount">Discount:</label>
-        <input type="number" name="discount" id="discount"
-            value="{{ old('discount', $studentFee->discount ?? 0) }}">
-    </div>
-</div>
-
-<div class="form-row">
     <div class="form-group">
         <label for="payment">Payment:</label>
         <input type="number" name="payment" id="payment"
             value="{{ old('payment', $studentFee->payment ?? 0) }}">
     </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group">
+        <label for="discount">Discount:</label>
+        <input type="number" name="discount" id="discount"
+            value="{{ old('discount', $studentFee->discount ?? 0) }}">
+    </div>
     <div class="form-group">
         <label for="dues">Dues:</label>
         <input type="number" name="dues" id="dues" value="{{ old('dues', $studentFee->dues ?? 0) }}"
-            readonly>
+            disabled>
     </div>
 </div>
 
